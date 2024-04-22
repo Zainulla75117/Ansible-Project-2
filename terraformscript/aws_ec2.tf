14b0aedbb251c0150ad2c386a44a29175351c05e
Zainulla75117 <pzainullabideen@gmail.com>
2024-04-22 14:45:45 +0530

    updated terraform file

--- terraformscript/aws_ec2.tf
+++ terraformscript/aws_ec2.tf
@@ -1,13 +1,14 @@
-provider "aws"{
-region="eu-west-2"
-}//provider End
+provider "aws" {
+  region = "eu-west-2"
+}
 
-resource"aws-instance" "AWSEC2"{
-ami="ami-019a292cfb114a776"
-instance_type="t2.micro"
-key_name="Zain"
-security_groups=["launch-wizard-3"]
-tags={
-    Name="TerraformAnsible"
-}//tags  End
-}//resource End
+resource "aws_instance" "AWSEC2" {
+  ami           = "ami-019a292cfb114a776"
+  instance_type = "t2.micro"
+  key_name      = "Zain"
+  security_groups = ["launch-wizard-3"]
+
+  tags = {
+    Name = "TerraformAnsible"
+  }
+}
