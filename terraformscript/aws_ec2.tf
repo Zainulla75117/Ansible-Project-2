c96766bfb3cb16067315930682619c3df9b8de2b
Zainulla75117 <pzainullabideen@gmail.com>
2024-04-22 14:30:00 +0530

    updated Jenkinsfile

--- terraformscript/aws_ec2.tf
+++ terraformscript/aws_ec2.tf
@@ -3,9 +3,9 @@ region="eu-west-2"
 }//provider End
 
 resource"aws-instance" "AWSEC2"{
-ami="019a292cfb114a776"
+ami="ami-019a292cfb114a776"
 instance_type="t2.micro"
-key_name="Zain.pem"
+key_name="Zain"
 security_groups=["launch-wizard-3"]
 tags={
     Name="TerraformAnsible"
